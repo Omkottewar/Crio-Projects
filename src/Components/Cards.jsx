ad2cde3952c774463fbeb12c290197139e115574
Omkottewar <omkottewar19.04@gmail.com>
2024-10-01 21:47:03 +0530

    second commit

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = ({data}) => {
 
 
   return (
-    <div className=' flex flex-col text-center  p-5 border border-gray-300 rounded-md hover:w-52'>
+    <div className=' flex flex-col text-center  p-5 border border-gray-300 rounded-md hover:opacity-70 shadow-lg cursor-pointer'>
         <img className='w-40 h-32 ' src={flag} alt="" />
         <h3>{(name).slice(0,15)}</h3>
     </div>
